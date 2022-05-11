3cb15ae8f76e65496434556a2743b96278c473ef
Fatih Uyvar Turkel <103960526+fatihuturkel@users.noreply.github.com>
2022-05-11 07:47:03 +0300

    Add files via upload
    login.html dosyasının bulunamaması durumunda yeni path eklendi.

--- giris.php
+++ giris.php
@@ -1,19 +1,35 @@
 <?php
     $kullanıcı="G211210016@sakarya.edu.tr";
     $sifre="G211210016";
+    $path='./login.html';  /* dosya aynı klasörde bulunuyorsa*/
+    $path2='../login.html'; /* dosya bir üst klasörde bulunuyorsa*/
 
     $_Email=htmlspecialchars($_POST['email']);
     $pass=htmlspecialchars( $_POST['sifre']);
 
     if($_POST['gonder']){
-        if(empty($_Email)or empty($pass)){
-            header('Location: /login.html');
-        }
-        else if($_Email==$kullanıcı and $pass==$sifre){
-            echo 'Hoşgeldiniz ' ,$sifre; 
+        if(file_exists($path)){
+            if(empty($_Email)or empty($pass)){
+                header ('Location: /web/login.html');
+            }
+            else if($_Email==$kullanıcı and $pass==$sifre){
+                echo 'Hoşgeldiniz ' ,$sifre; 
+            }
+            else{
+                header ('Location: /web/login.html');
+            }
         }
         else{
-            header('Location: /login.html');
+            if(empty($_Email)or empty($pass)){
+                header ('Location: /login.html');
+            }
+            else if($_Email==$kullanıcı and $pass==$sifre){
+                echo 'Hoşgeldiniz ' ,$sifre; 
+            }
+            else{
+                header ('Location: /login.html');
+            }
         }
+        
     }
 ?>
